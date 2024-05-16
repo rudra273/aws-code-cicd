bbda774342bea7df5108f6dfa9de79eabf4dedfe
rudra273 <rudramohanty45@gmail.com>
2024-05-16 13:13:27 +0530

    check deploye

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+# Get the ID of the running Docker container
+container_id=$(sudo docker ps -q --filter "status=running")
+
+if [ -n "$container_id" ]; then
+    echo "Stopping and removing container with ID: $container_id"
+    # Stop the container
+    sudo docker stop $container_id
+    # Remove the stopped container
+    sudo docker rm $container_id
+else
+    echo "No running container found."
+fi
